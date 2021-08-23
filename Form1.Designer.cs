bb90f3b8022accaca4c06334cf0a17d556592387
brunomotadev <brunomotadev@gmail.com>
2021-08-23 13:50:58 -0300

    Programa de Dias da Semana

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Switch_case
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSemana = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboSemana
+            // 
+            this.cboSemana.FormattingEnabled = true;
+            this.cboSemana.Items.AddRange(new object[] {
+            "Domingo",
+            "Segunda",
+            "Terça",
+            "Quarta",
+            "Quinta",
+            "Sexta",
+            "Sabado"});
+            this.cboSemana.Location = new System.Drawing.Point(169, 182);
+            this.cboSemana.Name = "cboSemana";
+            this.cboSemana.Size = new System.Drawing.Size(121, 23);
+            this.cboSemana.TabIndex = 0;
+            this.cboSemana.SelectedIndexChanged += new System.EventHandler(this.cboSemana_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escolha um dia da semana";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 392);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboSemana);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSemana;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_case
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cboSemana_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cboSemana.Text)
+            {
+                case "Domingo":
+                    MessageBox.Show("Você escolheu Domingo que é o 1° dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Segunda":
+                    MessageBox.Show("Você escolheu Segunda que é o 2° dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Terça":
+                    MessageBox.Show("Você escolheu Terça que é o 3° dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Quarta":
+                    MessageBox.Show("Você escolheu Quarta que é o 4° dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Quinta":
+                    MessageBox.Show("Você escolheu Quinta que é o 5° dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Sexta":
+                    MessageBox.Show("Você escolheu Sexta que é o 6° dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Sabado":
+                    MessageBox.Show("Você escolheu Sábado que é o 7° dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+            }
+        }
+    }
+}
